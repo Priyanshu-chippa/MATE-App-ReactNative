fbbc89885ae36dfc4fc78a7af7f8aad8bd1d99e5
Priyanshu Chippa <88324157+Priyanshu-chippa@users.noreply.github.com>
2025-06-08 18:41:33 +0530

    Updated firebase.js

--- firebase.js
+++ firebase.js
@@ -2,13 +2,24 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = {
+/*const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBA0SE_APP_ID,
+};
+*/
+
+// Add the new config with hardcoded values
+const firebaseConfig = {
+  apiKey: "AIzaSyDU6JzEN3QWz-CMED0RcKfPaxWbUliKE_8", // Paste your actual key here
+  authDomain: "mate-appbolt.firebaseapp.com", // Paste your actual domain here
+  projectId: "mate-appbolt", // Paste your actual project ID here
+  storageBucket: "mate-appbolt.firebasestorage.app", // etc.
+  messagingSenderId: "125842862724",
+  appId: "1:125842862724:web:bad40bf72beed0702548d1",
 };
 
 // Initialize Firebase
